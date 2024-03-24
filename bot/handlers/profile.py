9913928cc5ae5f605cc257f61559f1c374332ff5
mshrnv <cool.shoronov19@gmail.com>
2024-03-24 09:55:01 +0300

    Some changes

--- bot/handlers/profile.py
+++ bot/handlers/profile.py
@@ -2,8 +2,8 @@ from aiogram import Router, types
 from aiogram import F
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from bot.keyboards.inline import profile_keyboard, history_keyboard
-from bot.services.users import get_user_balance
+from bot.keyboards.inline import profile_keyboard
+from bot.services.users import get_user_balances
 
 router = Router(name="profile")
 
@@ -11,17 +11,10 @@ router = Router(name="profile")
 @router.callback_query(F.data == "profile")
 async def profile_handler(callback: types.CallbackQuery, session: AsyncSession) -> None:
     """Profile info"""
-    balance = await get_user_balance(session, callback.from_user.id)
+    # TODO: Get correct balance (Sum of all wallets)
+    balances = await get_user_balances(session, callback.from_user.id)
 
-    text = f"👤<b>Информация о профиле</b>\n\n<b>ID:</b> {callback.from_user.id}\n<b>Баланс:</b> {balance} USDT"
+    text = f"👤<b>Информация о профиле</b>\n\n<b>ID:</b> {callback.from_user.id}\n<b>Общий баланс:</b> {balances['summary_balance']} USDT\n\n<b>Баланс торгового счета</b>: {balances['trading_balance']} USDT\n<b>Баланс депозитных счетов</b>: {balances['deposit_balance']} USDT\n<b>Баланс счетов для вывода</b>: {balances['withdraw_balance']} USDT"
 
     await callback.message.edit_text(text, reply_markup=profile_keyboard())
-    await callback.answer()
-
-
-@router.callback_query(F.data == "history")
-async def history_handler(callback: types.CallbackQuery) -> None:
-    """History info"""
-    await callback.message.edit_text("📝<b>История транзакций</b>\n\nВы пока не совершили ни одной транзакции",
-                                     reply_markup=history_keyboard())
     await callback.answer()
--- bot/services/users.py
+++ bot/services/users.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Dict
 
 from sqlalchemy import select, insert, update
+from sqlalchemy.sql import func
 
-from bot.database.models import User, Transaction
+from bot.database.models import User, Transaction, Wallet
 
 if TYPE_CHECKING:
     from aiogram.types import User
@@ -45,15 +46,35 @@ async def user_exists(session: AsyncSession, user_id: int) -> bool:
     return bool(user)
 
 
-async def get_user_balance(session: AsyncSession, user_id: int) -> int:
-    """Returns user's balance"""
+async def get_user_balances(session: AsyncSession, user_id: int) -> Dict[Any]:
+    """Returns user's balances"""
+
+    balances = {
+        'trading_balance': 0,
+        'deposit_balance': 0,
+        'withdraw_balance': 0,
+        'summary_balance': 0
+    }
+
     query = select(User.balance).filter_by(id=user_id).limit(1)
-
     result = await session.execute(query)
-
     balance = result.scalar_one_or_none()
+    balances['trading_balance'] = balance if balance else 0
 
-    return balance
+    query = select(func.sum(Wallet.balance)).filter_by(user_id=user_id, is_deposit=True).limit(1)
+    result = await session.execute(query)
+    balance = result.scalar_one_or_none()
+    balances['deposit_balance'] = balance if balance else 0
+
+    query = select(func.sum(Wallet.balance)).filter_by(user_id=user_id, is_deposit=False).limit(1)
+    result = await session.execute(query)
+    balance = result.scalar_one_or_none()
+    balances['withdraw_balance'] = balance if balance else 0
+
+    balances['summary_balance'] = balances['trading_balance'] + balances['deposit_balance'] + balances[
+        'withdraw_balance']
+
+    return balances
 
 
 async def deposit(session: AsyncSession, user_id: int, amount: int, wallet: str) -> bool:
@@ -90,3 +111,26 @@ async def withdraw(session: AsyncSession, user_id: int, amount: int, wallet: str
     await session.commit()
 
     return True
+
+
+async def change_notifications_status(session: AsyncSession, user_id: int) -> bool:
+    """Changes user's notifications status"""
+
+    query = select(User.notifications).filter_by(id=user_id).limit(1)
+    result = await session.execute(query)
+    notifications_status = result.scalar_one_or_none()
+
+    query = update(User).where(User.id == user_id).values(notifications=not notifications_status)
+    result = await session.execute(query)
+    await session.commit()
+
+    return not notifications_status
+
+
+async def get_notifications_status(session: AsyncSession, user_id: int) -> bool:
+    """Changes user's notifications status"""
+    query = select(User.notifications).filter_by(id=user_id).limit(1)
+    result = await session.execute(query)
+    notifications_status = result.scalar_one_or_none()
+
+    return notifications_status
