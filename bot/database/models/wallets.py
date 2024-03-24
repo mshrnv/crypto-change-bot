ad5de8382d51109c376b5c71079cc46510515b82
mshrnv <cool.shoronov19@gmail.com>
2024-03-24 09:54:00 +0300

    Wallet model

---
+++ bot/database/models/wallets.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import Annotated
+
+from sqlalchemy import ForeignKey, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+int_pk = Annotated[int, mapped_column(primary_key=True, unique=True, autoincrement=True)]
+user_id_fk = Annotated[int, mapped_column(ForeignKey('users.id'))]
+
+
+class Wallet(Base):
+    __tablename__ = "wallets"
+
+    id: Mapped[int_pk]
+    user_id: Mapped[user_id_fk]
+    is_deposit: Mapped[bool]
+    balance: Mapped[int]
+    hex_address: Mapped[str]
+    base58_address: Mapped[str]
+    public_key: Mapped[str]
+    private_key: Mapped[str]
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
