428679346801bb30e379534a77c62b7c84b33222
mshrnv <cool.shoronov19@gmail.com>
2024-03-24 09:54:12 +0300

    Loader changed

--- bot/core/loader.py
+++ bot/core/loader.py
@@ -4,8 +4,7 @@ from aiogram.enums import ParseMode
 from .config import settings
 
 token = settings.BOT_TOKEN
+DEBUG = settings.DEBUG
 
 bot = Bot(token=token, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
-
-DEBUG = settings.DEBUG
