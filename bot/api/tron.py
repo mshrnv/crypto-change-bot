bfe45822eae6f11c7a7751f829ab7398fa618443
mshrnv <cool.shoronov19@gmail.com>
2024-03-24 11:26:36 +0300

    Getting balance

--- bot/api/tron.py
+++ bot/api/tron.py
@@ -1,7 +1,9 @@
+import aiohttp
 from tronpy import AsyncTron
 from tronpy.keys import PrivateKey
 
 
+# TODO: To delete?
 async def transfer_trx(_from, _to, _amount, _private_key):
     async with AsyncTron(network='nile') as client:
         txb = (
@@ -34,3 +36,20 @@ async def transfer_usdt(_from, _to, _amount, _private_key):
         tx = await client.broadcast(transfer_data)
 
         return tx
+
+
+async def get_wallet_balance(address):
+    usdt_contract_address = "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj"
+
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+        url = f"https://nile.trongrid.io/v1/accounts/{address}"
+        async with session.get(url) as resp:
+            data = await resp.json()
+            trc20 = data.get('data')[0].get('trc20')
+
+            for token in trc20:
+                for contract, balance in token.items():
+                    if contract == usdt_contract_address:
+                        return float(balance) / (10 ** 6)
+
+        return 0
