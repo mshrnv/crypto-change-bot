07cf52fb08a7de0809093cb90088818d1e264d2f
mshrnv <cool.shoronov19@gmail.com>
2024-03-24 09:53:14 +0300

    Callback Factories

--- bot/data/callback.py
+++ bot/data/callback.py
@@ -1,5 +1,21 @@
 from aiogram.filters.callback_data import CallbackData
 
 
-class DepositCallbackFactory(CallbackData, prefix="deposit"):
-    address: str
+class DepositWalletCallbackFactory(CallbackData, prefix="depo_wall"):
+    wallet_id: int
+
+
+class WithdrawWalletCallbackFactory(CallbackData, prefix="withdraw_wall"):
+    wallet_id: int
+
+
+class DeleteDepositWalletCallbackFactory(CallbackData, prefix="delete_depo_wall"):
+    wallet_id: int
+
+
+class DeleteWithdrawWalletCallbackFactory(CallbackData, prefix="delete_with_draw_wall"):
+    wallet_id: int
+
+
+class WithdrawCallbackFactory(CallbackData, prefix="wthdrw"):
+    wallet_id: int
